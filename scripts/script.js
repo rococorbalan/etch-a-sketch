9e0fe919951e921b97e0fa73ae7aa228ae7a2a7f
Rocoso <corbaroco@gmail.com>
2024-04-19 05:56:35 -0300

    Add color selector, clear button and rainbow mode
    Color selector for changing pencilColor, clear
    button for clearing all the canvas at once by
    iterating each pixel and changing it to white and
    rainbow mode using math to get a new hex color
    every click.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 const container = document.querySelector("#container");
 const input = document.querySelector(".size")
-const pixels = document.querySelectorAll(".pixel")
+let pixels = container.children
+const color = document.querySelector(".color")
+const clear = document.querySelector(".clear")
+const rainbow = document.querySelector(".rainbow")
 
 let mouseDown = false;
 document.body.onmousedown = () => {
@@ -10,7 +13,23 @@ document.body.onmouseup = () => {
     mouseDown = false;
 }
 
+let pencilColor = "#000000"
 
+color.addEventListener("input", () =>{
+    pencilColor = color.value;
+    console.log(pencilColor)
+})
+
+let rainbowMode = false;
+
+clear.addEventListener("click", clearCanvas)
+rainbow.addEventListener("click", () => {
+    if (rainbowMode == true) {
+        rainbowMode = false;
+    }else if (rainbowMode == false) {
+        rainbowMode = true;
+    }
+})
 
 function createGrid(rows, columns) {
     
@@ -18,7 +37,12 @@ function createGrid(rows, columns) {
         let cell = document.createElement("div");
         cell.addEventListener("mouseover", () => {
             if (mouseDown) {
-                cell.style.backgroundColor = 'black';
+                if (rainbowMode){
+                    cell.style.backgroundColor = randomHexColorCode();
+                }
+                if (!rainbowMode){
+                    cell.style.backgroundColor = pencilColor;
+                }
             }
         })
         cell.className = "pixel"
@@ -28,5 +52,15 @@ function createGrid(rows, columns) {
 
 createGrid(16, 16)
 
+function clearCanvas() {
+    console.log("cleared")
+    for (let i = 0; i < pixels.length; i++){
+        let pixel = pixels[i]
+        pixel.style.backgroundColor = "white"
+    }
+}
 
-
+function randomHexColorCode () {
+    let n = (Math.random() * 0xfffff * 1000000).toString(16);
+    return '#' + n.slice(0, 6);
+}
